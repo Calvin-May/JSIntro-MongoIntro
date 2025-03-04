33be581a8181e130ea20f641b1599659b245ecf0
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-04 07:44:20 -0500

    Updated router to handle additional pages, reenabled link highlighting

--- routes/index.ts
+++ routes/index.ts
@@ -3,12 +3,42 @@ var router = express.Router();
 
 /* Get Default Route */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Home'})
+  res.render('index', {title: 'Home', page: 'home'})
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Us', page: 'about' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Our Projects', page: 'projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Our Services', page: 'services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact Us', page: 'contact' });
+});
+
+/* GET Login page. */
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Login', page: 'login' });
+});
+
+/* GET Register page. */
+router.get('/register', function(req, res, next) {
+  res.render('index', { title: 'Register', page: 'register' });
 });
 
 module.exports = router;
