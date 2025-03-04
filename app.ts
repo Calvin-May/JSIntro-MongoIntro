893c801f3a37e555597d9bd7f086e2143cb0142c
Calvin May <calvinkylemay.dev@gmail.com>
2025-03-04 08:11:59 -0500

    Error page now has loads ejs partial for 404 errors

--- app.ts
+++ app.ts
@@ -41,7 +41,7 @@ app.use(function(err:createError.HttpError, req:express.Request, res:express.Res
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { message: message, error: error } );
+  res.render('error', { message: message, error: error, title: 'Error', page: 'error' } );
 });
 
 module.exports = app;
